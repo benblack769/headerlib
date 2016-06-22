68f4990d48a308482a079fc20a5d757f227f8e78
benblack <benblack@c031h133.wless.reed.edu>
2016-06-21 23:07:33 -0700

    updated zip-for

--- zip_for.hpp
+++ zip_for.hpp
@@ -30,46 +30,16 @@ Function zip_range(Function func, ContTy1 Cont1,ContTy2 Cont2){
 template <typename Function, typename ItTy1,typename ItTy2>
 void for_each (ItTy1 begin1, ItTy1 end1, ItTy2 begin2, ItTy2 end2,Function func){
 	for (;begin1 != end1 && begin2 != end2; ++begin1,++begin2){
-		func(*begin1,*begin2):
+                func(*begin1,*begin2);
 	}
 }
-template <typename Function, typename ItTy1,typename ItTy2,typename ItTy2>
-void for_each (ItTy1 begin1, ItTy1 end1, ItTy2 begin2, ItTy2 end2,Function func){
-	for (;begin1 != end1 && begin2 != end2; ++begin1,++begin2){
-		func(*begin1,*begin2):
+template <typename Function, typename ItTy1,typename ItTy2,typename ItTy3>
+void for_each (ItTy1 begin1, ItTy1 end1, ItTy2 begin2, ItTy2 end2, ItTy3 begin3, ItTy3 end3,Function func){
+        for (;begin1 != end1 && begin2 != end2 && begin3 != end3; ++begin1,++begin2,++begin3){
+                func(*begin1,*begin2,*begin3);
 	}
 }
-
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <tuple>
-using namespace std;
-
-int main(){
-	/*unordered_set<int> a{0,1,2,3,4};
-	vector<double> b{0.0,0.1,0.2,0.3,0.4,0.5,0.6};
-	vector<char> c{'a','b','c','d','e','f','h'};
-	zip_for_each([](int & a_val,float b_val,char c_val){
-		cout << a_val << " " << b_val << " " << c_val << "\n";
-	},a.begin(),a.end(),b.rbegin(),b.rend(),c.begin(),c.end());
-
-	cout << "\n";
-
-	zip_range([](int & a_val,float b_val){
-		cout << a_val << " " << b_val << "\n";
-	},a,b);*/
-	int myint;
-  char mychar;
-
-  std::tuple<int,float,char> mytuple;
-
-  mytuple = std::make_tuple (10, 2.6, 'a');          // packing values into tuple
-
-  std::tie (myint, std::ignore, mychar) = mytuple;   // unpacking tuple into variables
-
-  std::cout << "myint contains: " << myint << '\n';
-  std::cout << "mychar contains: " << mychar << '\n';
-
-	return 0;
+template<typename Function,typename ContTy1,typename ContTy2>
+Function zip_range(ContTy1 Cont1,ContTy2 Cont2,Function func){
+    return for_each(Cont1.begin(),Cont1.end(),Cont2.begin(),Cont2.end(),func);
 }
